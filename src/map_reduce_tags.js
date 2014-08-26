37a34f365f4e11c019e27997f7228e6dfc64695e
Ivan Pedrazas <ipedrazas@gmail.com>
2014-08-26 23:26:34 +0100

    map reduce js instructions to create the tags service

---
+++ src/map_reduce_tags.js
@@ -0,0 +1,23 @@
+var map = function() {
+    var tags = this.tags;
+    if (tags) {
+        for (var i = tags.length - 1; i >= 0; i--) {
+            if (tags[i])  {      // make sure there's something
+               emit(tags[i].toLowerCase(), 1); // store a 1 for each word
+            }
+        }
+    }
+};
+
+
+var reduce = function( key, values ) {
+    var count = 0;
+    values.forEach(function(v) {
+        count +=v;
+    });
+    return count;
+};
+
+
+
+db.dotmarks.mapReduce(map, reduce, {out: "tag_count"})
