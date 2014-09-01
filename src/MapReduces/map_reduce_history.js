c365f2a308e0684590862d84f48befdef642dc02
Ivan Pedrazas <ipedrazas@gmail.com>
2014-09-01 10:46:36 +0100

    mapreduce scripts added

--- src/MapReduces/map_reduce_history.js
+++ src/MapReduces/map_reduce_history.js
@@ -42,3 +42,63 @@ var reduceHistoryByHoursPerUser = function( key, values ) {
 
 
 db.history.mapReduce(mapHistoryByHoursPerUser, reduceHistoryByHoursPerUser, {out: "ivan_per_hours"});
+
+
+// MapReduce to extract counts per weekday
+
+var mapHistoryPerWeekdayPerUser = function() {
+    if(this.user == "ivan"){
+            emit(this.time.weekday, 1); // store a 1 for each word
+    }
+};
+
+
+var reduceHistoryPerWeekdayPerUser = function( key, values ) {
+    var count = 0;
+    values.forEach(function(v) {
+        count +=v;
+    });
+    return count;
+};
+
+
+
+db.history.mapReduce(mapHistoryPerWeekdayPerUser, reduceHistoryPerWeekdayPerUser, {out: "ivan_per_weekday"});
+
+
+
+
+// MapReduce to extract counts per domain
+
+db.system.js.save(
+   {
+     _id : "cleanUp" ,
+     value : function(url) {
+                if(url.search(/^https?\:\/\//) != -1)
+                    url = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i, "");
+                else
+                    url = url.match(/^([^\/?#]+)(?:[\/?#]|$)/i, "");
+                url[1] = url[1].replace(/^www\./i, "");
+                return url[1];
+            }
+   }
+);
+
+var mapHistoryPerDomainPerUser = function() {
+    if(this.user == "ivan"){
+            emit(cleanUp(this.url), 1); // store a 1 for each word
+    }
+};
+
+
+var reduceHistoryPerDomainPerUser = function( key, values ) {
+    var count = 0;
+    values.forEach(function(v) {
+        count +=v;
+    });
+    return count;
+};
+
+
+
+db.history.mapReduce(mapHistoryPerDomainPerUser, reduceHistoryPerDomainPerUser, {out: "ivan_per_domain"});
